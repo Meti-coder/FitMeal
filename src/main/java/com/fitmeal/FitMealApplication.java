8330fff9246896242185104086cb090331f2c908
Kanakappa Meti <MSS-121@modus.local1>
2025-09-24 11:58:39 +0530

    first commit-project structure

---
+++ src/main/java/com/fitmeal/FitMealApplication.java
@@ -0,0 +1,13 @@
+package com.fitmeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitMealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitMealApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fitmeal/FitMealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitmeal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitMealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
